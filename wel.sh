63a88e4503099e2d8ed4fbfff4cb8c858fca379e
anand10062000 <anandkumarramisetti@gmail.com>
2021-08-22 08:13:39 +0530

    emp daily wage

--- wel.sh
+++ wel.sh
@@ -1,11 +1,14 @@
 #! /bin/bash
-
+dailywage=20
+fullday=8
 echo "welcome"
 rannum=$((RANDOM%2))
 echo $rannum
 if [[ $rannum -eq 1 ]]
 then 
 echo "present"
+dailyempwage=$(($dailywage * $fullday))
+echo $dailyempwage
 else
 echo "absent"
 fi

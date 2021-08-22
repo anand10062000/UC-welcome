8a01c5e67539b75bfd9819fda2217a7bf07539e9
anand10062000 <anandkumarramisetti@gmail.com>
2021-08-22 08:33:37 +0530

    parttime wage

--- wel.sh
+++ wel.sh
@@ -1,14 +1,20 @@
 #! /bin/bash
 dailywage=20
 fullday=8
+parttime=4
 echo "welcome"
-rannum=$((RANDOM%2))
+rannum=$((RANDOM%2+1))
 echo $rannum
 if [[ $rannum -eq 1 ]]
 then 
 echo "present"
 dailyempwage=$(($dailywage * $fullday))
 echo $dailyempwage
+elif [[ $rannum -eq 2 ]]
+then 
+echo "parttime"
+parttimewage=$(( $dailywage * $parttime ))
+echo $parttimewage
 else
 echo "absent"
 fi
